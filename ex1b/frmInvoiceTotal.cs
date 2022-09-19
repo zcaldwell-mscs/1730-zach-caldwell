9b9748ea1c59bb32bde347ee1a92470adf0e6324
Zachary Caldwell <zo8245sk@go.minnstate.edu>
2022-09-19 04:03:15 -0500

    changes

---
+++ ex1b/frmInvoiceTotal.cs
@@ -0,0 +1,25 @@
+namespace ex1b
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
